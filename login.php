f17b10d6cef951fadad6751038bb8ef90b3035db
Joshua Mugasu <162455896+Mimikell@users.noreply.github.com>
2024-12-31 21:43:09 +0300

    Update login.php

--- login.php
+++ login.php
@@ -16,17 +16,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($connect, $query);
+    $query = $connect->prepare("SELECT * FROM users WHERE username=? AND password=?");
+    $paramTypes = "ss"; 
+    $query->bind_param($paramTypes, $username, $password);
+    $query->execute();
+    $result = $query->get_result();
 
     if (mysqli_num_rows($result) > 0) {
         $_SESSION['username'] = $username;
-        header("Location:success.html");
+        header("Location: success.html");
         exit();
     } else {
-        header("Location:index.html?error=Invalid username or password");
+        header("Location: index.html?error=Invalid username or password");
         exit();
     }
+
+    $query->close();
 }
 
 mysqli_close($connect);
